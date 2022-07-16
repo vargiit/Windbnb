047e5044f6c0d44c317bb14ed5b67ced10e31864
vargiit <94285586+vargiit@users.noreply.github.com>
2022-07-16 16:47:25 +0530

    Guest input updated

--- src/windBnb/DisplayCount.js
+++ src/windBnb/DisplayCount.js
@@ -1,13 +1,21 @@
 import Counter from "./Counter";
 import { useState } from "react";
+import "./DisplayCount.css";
 
 function DisplayCount() {
   let [counter, setCounter] = useState(0);
   let [counter2, setCounter2] = useState(0);
+  let totalGuests = counter + counter2;
   return (
     <div>
-      <label htmlFor="guests"></label>
-      <input id="guests" type="text" value={counter + counter2 + " Guests"} />
+      <div className="float-container">
+        <label htmlFor="guests">Guests</label>
+        <input
+          id="guests"
+          type="text"
+          value={totalGuests > 0 ? totalGuests + " guests" : ""}
+        />
+      </div>
       <Counter childrens="Adults" counter={counter} setCounter={setCounter}>
         Age 13 or above
       </Counter>
